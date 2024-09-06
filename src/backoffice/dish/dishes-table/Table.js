4a4d4459c9ad7cfd393f9f9e887db0bb02902a2c
juansanchezilab <juan.sanchez@ilab.digital>
2024-09-05 23:45:00 -0500

    Se consolida servicio de actulizacón de producto en un solo componente

--- src/backoffice/dish/dishes-table/Table.js
+++ src/backoffice/dish/dishes-table/Table.js
@@ -8,150 +8,9 @@ import { ProductsContext } from "../../context/restaurant/ProductsContext";
 import { Button } from 'primereact/button';
 import { MdOutlineEdit } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
-import { Dialog } from 'primereact/dialog';
+import { Dialog } from "primereact/dialog";
 import { DishForm } from "../dish-form/DishForm";
 
-const UpdateDishPanel = ({ visibility, setVisibility, dishId }) => {
-
-    // Env
-    const BASE_URL = process.env.REACT_APP_URL;
-
-    // Context
-    const tokenContext = useContext(TokenContext);
-    const { token } = tokenContext;
-
-    // State
-    const [dish, setDish] = useState(null);
-
-    const productsContext = useContext(ProductsContext);
-    const { setProducts } =  productsContext; 
-
-    useEffect(() => {
-        function getDish(){
-            fetch(BASE_URL + '/dish/get/' + dishId, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'authorization': 'Bearer ' + token,
-                }
-            }).then((response) => {
-                if(!response.ok){
-                    throw new Error('No fue posible consultar el producto.')
-                }
-                return response.json();
-            }).then((data) => {
-                console.log('data', data);
-                if(data){
-                    setDish(data);
-                }
-            }).catch((error) => {
-                console.error(error);
-            });
-        }
-        if(token){
-            getDish();
-        }
-    }, [dishId, token]);
-
-    const updateDish = (name, price, category, description, image = 'image') => {
-        const data = {};
-        if(name){
-            data.name = name;
-        }
-
-        if(price){
-            data.price = price;
-        }
-
-        if(category && category.categoryId){
-            data.categoryId = category.categoryId;
-        }
-
-        if(description){
-            data.description = description;
-        }
-
-        if(image){
-            data.image = image;
-        }
-
-        fetch(BASE_URL + '/dish/update', {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': 'Bearer ' + token,
-            },
-            body: JSON.stringify({
-                data, 
-                dishId
-            }),
-        }).then((response) => {
-            if(!response.ok){
-                throw new Error('No fue posible consultar el producto.')
-            }
-            return response.json();
-        }).then((data) => {
-            console.log('data', data);
-            getDishes();
-            setVisibility(false);
-        }).catch((error) => {
-            console.error(error);
-        });
-
-        function getDishes(){
-            fetch(BASE_URL + '/dish/get-all', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'authorization': 'Bearer ' + token
-                },
-            }).then((response) => {
-                if(response.ok){
-                    return response.json();
-                }
-            }).then((data) => {
-                data.forEach((product) => {
-                    if(product && !product.description){
-                        product.description = 'Sin descripción'; 
-                    }
-                });
-                setProducts(data);
-            }).catch((error) => {
-                console.error(error); 
-            });
-        }
-    }
-
-    return(
-        <>
-        {   dish && dish.category && 
-            <Dialog 
-                header="Actualizar producto" 
-                visible={visibility} 
-                style={{ width: '50vw' }} 
-                onHide={() => {
-                if (!visibility) {
-                    return;
-                }else {
-                    setDish(null);
-                    setVisibility(false); }  
-                } 
-                }>
-                <DishForm
-                    buttonText={'ACTUALIZAR'} 
-                    showTitile={false} 
-                    showAddCategoryButton={false}
-                    action={updateDish} 
-                    givenName={dish.name}
-                    givenCategory={dish.category}
-                    givenDescription={dish.description}
-                    givenPrice={dish.price}
-                />
-            </Dialog>
-        }
-        </>
-    );
-}
 
 const Table = () => {
 
@@ -163,73 +22,140 @@ const Table = () => {
     const { token } = tokenContext;
     const productsContext = useContext(ProductsContext);
     const { setProducts, products } =  productsContext; 
-    const [dishes, setDishes] = useState([]);
+    
+    // States
     const [updateDishPanelVisibility, setUpdateDishPanelVisibility] = useState(false);
+    const [dishes, setDishes] = useState([]);
+    const [dish, setDish] = useState();
     const [dishId, setDishId] = useState(null);
 
-    useEffect(() => {
-        function sortProducts(){
-            const data = products;
-            // Se agrega formato a los precios
-            data.forEach((dish) => {
-                if(dish && dish.price){
-                    dish.price = '$' + formatCurrency(dish.price);
+    // Functions 
+
+    function getDish(){
+        fetch(BASE_URL + '/dish/get/' + dishId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'Bearer ' + token,
+            }
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error('No fue posible consultar el producto.')
+            }
+            return response.json();
+        }).then((data) => {
+            if(data){
+                setDish(data);
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    function sortProducts(){
+        const data = products;
+        // Se agrega formato a los precios
+        data.forEach((dish) => {
+            if(dish && dish.price){
+                dish.price = '$' + formatCurrency(dish.price);
+            }
+        });
+
+        // Se clasifican los platos según categoría
+        const sortedProducts = data.reduce((sortedDishes, dish) => {
+            const categoryId = dish.category.categoryId;
+            const categoryName = dish.category.name;
+            if(!sortedDishes[categoryId]){
+                sortedDishes[categoryId] = {
+                    categoryName, 
+                    dishes: []
+                };
+            }
+            sortedDishes[categoryId].dishes = [ ...sortedDishes[categoryId].dishes, dish];
+            return sortedDishes;
+        }, []);
+        setDishes(sortedProducts);
+    }
+
+    function getDishes(){
+        fetch(BASE_URL + '/dish/get-all', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'Bearer ' + token
+            },
+        }).then((response) => {
+            if(response.ok){
+                return response.json();
+            }
+        }).then((data) => {
+            data.forEach((product) => {
+                if(product && !product.description){
+                    product.description = 'Sin descripción'; 
                 }
             });
-    
-            // Se clasifican los platos según categoría
-            const sortedProducts = data.reduce((sortedDishes, dish) => {
-                const categoryId = dish.category.categoryId;
-                const categoryName = dish.category.name;
-                if(!sortedDishes[categoryId]){
-                    sortedDishes[categoryId] = {
-                        categoryName, 
-                        dishes: []
-                    };
-                }
-                sortedDishes[categoryId].dishes = [ ...sortedDishes[categoryId].dishes, dish];
-                return sortedDishes;
-            }, []);
-            setDishes(sortedProducts);
-        }
+            setProducts(data);
+        }).catch((error) => {
+            console.error(error); 
+        });
+    }
 
+    const updateDish = (name, price, category, description, image = 'image') => {
+        fetch(BASE_URL + '/dish/update', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'Bearer ' + token,
+            },
+            body: JSON.stringify({
+                data: {
+                    name,
+                    price,
+                    categoryId: category.categoryId,
+                    description,
+                    image
+                }, 
+                dishId
+            }),
+        }).then((response) => {
+            if(!response.ok){
+                throw new Error('No fue posible consultar el producto.')
+            }
+            return response.json();
+        }).then((data) => {
+            getDishes();
+            setDishId(null);
+            setDish(null);
+            setUpdateDishPanelVisibility(false);
+        }).catch((error) => {
+            console.error(error);
+        });
+    }
+
+    // Effects
+    useEffect(() => {
+        if(token){
+            getDish();
+            setUpdateDishPanelVisibility(true);
+        }
+    }, [dishId, token]);
+
+
+    useEffect(() => {
         if(products){
             sortProducts(); 
         }
     }, [products]);
 
     useEffect(() => {
-        function getDishes(){
-            fetch(BASE_URL + '/dish/get-all', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'authorization': 'Bearer ' + token
-                },
-            }).then((response) => {
-                if(response.ok){
-                    return response.json();
-                }
-            }).then((data) => {
-                data.forEach((product) => {
-                    if(product && !product.description){
-                        product.description = 'Sin descripción'; 
-                    }
-                });
-                setProducts(data);
-            }).catch((error) => {
-                console.error(error); 
-            });
-        }
-
         if(token){
             getDishes();
         }
-    }, [token, ]);
+    }, [token]);
 
+    // Actions
     const showUpdateDishPanel = (dishId) => {
         setDishId(dishId);
-        setUpdateDishPanelVisibility(true);
     }
 
     const buttonTemplate = (rowData) => {
@@ -246,15 +172,10 @@ const Table = () => {
 
     return(<div className="table__container">
         
-        <UpdateDishPanel 
-            visibility={updateDishPanelVisibility} 
-            setVisibility={setUpdateDishPanelVisibility}
-            dishId={dishId}
-        />
-
         {dishes && dishes.length > 0 && dishes.map((p) => {    
             return(
                 <div key={p.category}>
+                    { updateDishPanelVisibility ? 'sip': 'nop'}
                     <DataTable value={p.dishes} header={tableTitleTemplate(p.categoryName)} tableStyle={{ minWidth: '50rem' }} >
                         <Column style={{ width: '5%' }} body={buttonTemplate} header="Editar"></Column>
                         <Column style={{ width: '5%' }} body={deleteButtonTemplate} header="Eliminar"></Column>
@@ -262,6 +183,33 @@ const Table = () => {
                         <Column style={{ width: '25%' }} field="price" header="Precio"></Column>
                         <Column style={{ width: '40%' }} field="description" header="Descripción"></Column>
                     </DataTable>
+
+                    {
+                        dish && dish.category &&  
+                        <Dialog
+                        header="Actualizar producto" 
+                        visible={updateDishPanelVisibility} 
+                        style={{ width: '50vw' }} 
+                        onHide={() => {
+                        if (!updateDishPanelVisibility) {
+                            return;
+                        }else {
+                            setDish(null);
+                            setDishId(null);
+                            setUpdateDishPanelVisibility(false); 
+                        }}}>
+                            <DishForm
+                                buttonText={'ACTUALIZAR'} 
+                                showTitile={false} 
+                                showAddCategoryButton={false}
+                                action={updateDish} 
+                                givenName={dish.name}
+                                givenCategory={dish.category}
+                                givenDescription={dish.description}
+                                givenPrice={dish.price}
+                            />
+                        </Dialog>
+                    }
                 </div>
         )})}
     </div>);
