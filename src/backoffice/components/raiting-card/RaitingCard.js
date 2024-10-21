167f8b22ba59cf101ae23b35ebcc4084c11403cc
juansanchezilab <juan.sanchez@ilab.digital>
2024-10-20 21:35:56 -0500

    Se agregan tootips

--- src/backoffice/components/raiting-card/RaitingCard.js
+++ src/backoffice/components/raiting-card/RaitingCard.js
@@ -1,48 +1,39 @@
-import React, { useEffect } from "react";
+import React from "react";
 import './raitingCard.scss';
 import { Rating } from 'primereact/rating';
 import { ProgressBar } from 'primereact/progressbar';
-import { FaRegUser } from "react-icons/fa";
 import { FaChartBar } from "react-icons/fa";
-
+import { Tooltip } from 'primereact/tooltip';
 
 const RaitingCard = ({ score, max, oneStarts, twoStarts, threeStarts, fourStarts, fiveStarts }) => {
 
     return (<div className="raiting-card__container">
         <div className="raiting-card__icon">
-            <FaChartBar size={20} color="#06b6d4"/>
+            <FaChartBar size={20} color="#06b6d4" />
         </div>
         <div className="raiting-card__summary">
-            <p className="raiting-card__score">{ score }</p>
+            <p className="raiting-card__score">{score}</p>
             <Rating value={score} cancel={false} />
         </div>
         <div className="raiting-card__detail">
-            <div>
-                <p>1</p>
-                <ProgressBar showValue={false} value={(oneStarts * 100) / max}></ProgressBar>
-            </div>
-
-            <div>
-                <p>2</p>
-                <ProgressBar showValue={false} value={(twoStarts * 100) / max}></ProgressBar>
-            </div>
-
-            <div>
-                <p>3</p>
-                <ProgressBar showValue={false} value={(threeStarts * 100) / max}></ProgressBar>
-            </div>
-
-            <div>
-                <p>4</p>
-                <ProgressBar showValue={false} value={(fourStarts * 100) / max}></ProgressBar>
-            </div>
-
-            <div>
-                <p>5</p>
-                <ProgressBar showValue={false} value={(fiveStarts * 100) / max}></ProgressBar>
-            </div>
+            <Rate className={'raiting-card__one-starts'} starts={oneStarts} max={max}/>   
+            <Rate className={'raiting-card__two-starts'} starts={twoStarts} max={max}/>
+            <Rate className={'raiting-card__three-starts'} starts={threeStarts} max={max}/>
+            <Rate className={'raiting-card__four-starts'} starts={fourStarts} max={max}/>
+            <Rate className={'raiting-card__five-starts'} starts={fiveStarts} max={max}/>
         </div>
     </div>);
+}
+
+const Rate = ({className, starts, max}) => {
+    return (<>
+        <Tooltip position="left" target={'.' + className} content={`${starts} votaciones`} />
+        <div>
+            <p>5</p>
+            <ProgressBar className={className} showValue={false} value={(starts * 100) / max}></ProgressBar>
+        </div>
+    </>
+    );
 }
 
 export { RaitingCard };
--- src/backoffice/components/statistics/Statistics.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Statistics = () => {
-    return(<div>
-
-    </div>);
-}
-
-export { Statistics };
