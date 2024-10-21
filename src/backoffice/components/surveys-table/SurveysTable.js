939cb4e4ea6edcc6d6ecfe5829683a9a268ed912
juansanchezilab <juan.sanchez@ilab.digital>
2024-10-20 21:20:28 -0500

    Se genera tabla para visualizar encuestas

---
+++ src/backoffice/components/surveys-table/SurveysTable.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './surveysTable.scss';
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+
+const tableTitleTemplate = (text) => {
+    return <h2>{text}</h2>
+}
+
+
+const SurveysTable = ({ surveys }) => {
+    return(<div className="surveys-table__container">
+        <DataTable value={surveys} header={tableTitleTemplate('Encuestas')} tableStyle={{ minWidth: '50rem' }} emptyMessage={'No hay categorías'} size={'small'} stripedRows={true} >
+            <Column style={{ width: '20%' }} field="date" header="Fecha"></Column>
+            <Column style={{ width: '20%' }} field="time" header="Hora"></Column>
+            <Column style={{ width: '20%' }} field="score" header="Puntaje"></Column>
+            <Column style={{ width: '40%' }} field="comments" header="Comentarios"></Column>
+        </DataTable>
+    </div>);
+}
+
+export { SurveysTable };
--- src/backoffice/pages/survey/Survey.js
+++ src/backoffice/pages/survey/Survey.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { RaitingCard } from "../../components/raiting-card/RaitingCard";
 import './survey.scss';
 import { RespondentsCard } from "../../components/respondents-card/RespondentsCard";
+import { SurveysTable } from "../../components/surveys-table/SurveysTable";
 
 const Survey = () => {
     const [numberOfRespondants, setNumberOfRespondants] = useState(0);
@@ -12,6 +13,7 @@ const Survey = () => {
     const [fourStarts, setFourStarts] = useState();
     const [fiveStarts, setFiveStarts] = useState();
     const [max, setMax] = useState(1);
+    const [surveys, setSurveys] = useState([]);
 
     useEffect(() => {
         const scores = [1, 2, 3, 5, 10];
@@ -23,6 +25,22 @@ const Survey = () => {
         setFiveStarts(scores[4]);
         setScore((sum(scores))/5);
         setNumberOfRespondants(54);
+        setSurveys([{
+            date: '2024-10-10',
+            time: '10:00:12',
+            score: 3,
+            comments: 'Comida deliciosa'
+        }, {
+            date: '2024-10-10',
+            time: '10:00:12',
+            score: 3,
+            comments: 'Comida deliciosa'
+        }, {
+            date: '2024-10-10',
+            time: '10:00:12',
+            score: 3,
+            comments: 'Comida deliciosa'
+        }]);
     }, []);
 
     
@@ -53,6 +71,9 @@ const Survey = () => {
             fourStarts={fourStarts}
             fiveStarts={fiveStarts}/>
             <RespondentsCard numberOfRespondants={numberOfRespondants} />
+        </div>
+        <div className="survey__table-container">
+            <SurveysTable surveys={surveys}/>
         </div>
     </div>);
 }
