4468ad0f05ee21479900004c487f0be80020925a
juansanchezilab <juan.sanchez@ilab.digital>
2024-10-22 17:33:28 -0500

    Se agrega generación de imagenes de menú

--- src/backoffice/pages/codes/Codes.js
+++ src/backoffice/pages/codes/Codes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import './codes.scss';
 import { QRCodeCanvas } from "qrcode.react";
 import { Button } from "primereact/button";
@@ -6,8 +6,12 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
 import { MdError } from "react-icons/md";
 import { MenuContext } from "../../context/restaurant/MenuContext";
 import { Toolbar } from 'primereact/toolbar';
+import { toPng } from 'html-to-image';
 
 const Codes = () => {
+    const menuCode = useRef();
+    const surveyCode = useRef();
+
     const menuContext = useContext(MenuContext);
     const { menu } = menuContext;
 
@@ -23,10 +27,41 @@ const Codes = () => {
 
     const startContent = () => {
         return <div className="codes__button-container">
-            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} />} label="Descargar código del menú" />
-            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} size={20} />} label="Descargar código de la encuesta" />
+            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} />} label="Descargar código del menú" onClick={dowloadMenuCode}/>
+            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} size={20} />} label="Descargar código de la encuesta" onClick={dowloadSurveyCode}/>
         </div>;
     }
+
+    const dowloadMenuCode = () => {
+        if (menuCode.current) {
+          toPng(menuCode.current)
+            .then((dataUrl) => {
+              const link = document.createElement('a');
+              link.href = dataUrl;
+              link.download = 'my-component.png';
+              link.click();
+            })
+            .catch((err) => {
+              console.error('Error generating image:', err);
+            });
+        }
+      };
+
+      
+    const dowloadSurveyCode = () => {
+        if (surveyCode.current) {
+          toPng(surveyCode.current)
+            .then((dataUrl) => {
+              const link = document.createElement('a');
+              link.href = dataUrl;
+              link.download = 'my-component.png';
+              link.click();
+            })
+            .catch((err) => {
+              console.error('Error generating image:', err);
+            });
+        }
+      };
 
     return (<div >
 
@@ -35,7 +70,7 @@ const Codes = () => {
             
         </div>
         <div className="codes__container">
-            <div className="codes__code">
+            <div className="codes__code" ref={menuCode}>
                 {url && <div>
                     <h2>MENÚ</h2>
                     <QRCodeCanvas
@@ -44,11 +79,10 @@ const Codes = () => {
                         bgColor={"#ffffff"}
                         fgColor={"#000000"}
                         level={"H"}
-                    />
-                    <p>{url}</p></div>}
+                    /></div>}
                 {!surveyUrl && <div><MdError size={30} /> <h2> No hay un código QR disponible</h2></div>}
             </div>
-            <div className="codes__code">
+            <div className="codes__code" ref={surveyCode}>
                 {surveyUrl && <div>
                     <h2>ENCUESTAS</h2>
                     <QRCodeCanvas
@@ -57,8 +91,7 @@ const Codes = () => {
                         bgColor={"#ffffff"}
                         fgColor={"#000000"}
                         level={"H"}
-                    />
-                    <p>{surveyUrl}</p></div>}
+                    /></div>}
                 {!surveyUrl && <div><MdError size={30} /> <h2>No hay un código QR disponible</h2></div>}
             </div>
 
