82c943ffa634fc08fd950c93db1248246f5bafac
juansanchezilab <juan.sanchez@ilab.digital>
2024-10-22 17:27:48 -0500

    Se agrega toolbar y librería para descargar codigos qr

--- src/backoffice/pages/codes/Codes.js
+++ src/backoffice/pages/codes/Codes.js
@@ -5,7 +5,7 @@ import { Button } from "primereact/button";
 import { IoCloudDownloadOutline } from "react-icons/io5";
 import { MdError } from "react-icons/md";
 import { MenuContext } from "../../context/restaurant/MenuContext";
-
+import { Toolbar } from 'primereact/toolbar';
 
 const Codes = () => {
     const menuContext = useContext(MenuContext);
@@ -21,34 +21,47 @@ const Codes = () => {
         setSurveyUrl(`${PAGE_URL}${menu.url}/encuesta`);
     }, [menu]);
 
-    return (<div className="codes__container">
+    const startContent = () => {
+        return <div className="codes__button-container">
+            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} />} label="Descargar código del menú" />
+            <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} size={20} />} label="Descargar código de la encuesta" />
+        </div>;
+    }
+
+    return (<div >
+
         <div>
-            {url && <div>
-                <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} />} label="Descargar" />
-                <h2>MENÚ</h2>
-                <QRCodeCanvas
-                    value={url}
-                    size={300}
-                    bgColor={"#ffffff"}
-                    fgColor={"#000000"}
-                    level={"H"}
-                />
-                <p>{url}</p></div>}       
-                {!surveyUrl && <div><MdError size={30} /> <h2> No hay un código QR disponible</h2></div>}
+            <Toolbar start={startContent}/>
+            
         </div>
-        <div>
-            {surveyUrl && <div>
-                <Button icon={<IoCloudDownloadOutline style={{ marginRight: '12px' }} size={20} />} label="Descargar" />
-                <h2>ENCUESTAS</h2>
-                <QRCodeCanvas
-                    value={surveyUrl}
-                    size={300}
-                    bgColor={"#ffffff"}
-                    fgColor={"#000000"}
-                    level={"H"}
-                />
-                <p>{surveyUrl}</p></div>}
-            {!surveyUrl && <div><MdError size={30} /> <h2>No hay un código QR disponible</h2></div>}
+        <div className="codes__container">
+            <div className="codes__code">
+                {url && <div>
+                    <h2>MENÚ</h2>
+                    <QRCodeCanvas
+                        value={url}
+                        size={300}
+                        bgColor={"#ffffff"}
+                        fgColor={"#000000"}
+                        level={"H"}
+                    />
+                    <p>{url}</p></div>}
+                {!surveyUrl && <div><MdError size={30} /> <h2> No hay un código QR disponible</h2></div>}
+            </div>
+            <div className="codes__code">
+                {surveyUrl && <div>
+                    <h2>ENCUESTAS</h2>
+                    <QRCodeCanvas
+                        value={surveyUrl}
+                        size={300}
+                        bgColor={"#ffffff"}
+                        fgColor={"#000000"}
+                        level={"H"}
+                    />
+                    <p>{surveyUrl}</p></div>}
+                {!surveyUrl && <div><MdError size={30} /> <h2>No hay un código QR disponible</h2></div>}
+            </div>
+
         </div>
     </div>);
 }
